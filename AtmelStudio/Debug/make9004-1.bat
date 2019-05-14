caa372247779104a0fc0df73ff95f55922e54ba9
Jakub Przybytek <jakub.przybytek@gmail.com>
2019-05-14 18:07:23 +0200

    clean up

--- AtmelStudio/Debug/make9004-1.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files (x86)\Atmel\Atmel Toolchain\AVR8 GCC\Native\3.4.1061\avr8-gnu-toolchain\bin\avr-objcopy.exe" -O ihex -R .eeprom -R .fuse -R .lock -R .signature -R .user_signatures  "MusicAmp.elf" "MusicAmp.hex"
